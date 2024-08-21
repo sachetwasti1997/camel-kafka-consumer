ecd4f7f063c7552ddea1cead7379d51508e2fa9e
sachetwasti1997 <sachetwasti61@gmail.com>
2024-08-21 11:05:44 +0530

    Made Kafka Consumer made routes and put a login processor

--- src/main/java/com/sachet/camel_microservice_II/routes/KafkaConsumerRoute.java
+++ src/main/java/com/sachet/camel_microservice_II/routes/KafkaConsumerRoute.java
@@ -1,13 +1,22 @@
 package com.sachet.camel_microservice_II.routes;
 
+import com.sachet.camel_microservice_II.utility.LoggingProcessor;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaConsumerRoute extends RouteBuilder {
+
+    private final LoggingProcessor processor;
+
+    public KafkaConsumerRoute(LoggingProcessor processor) {
+        this.processor = processor;
+    }
+
     @Override
     public void configure() throws Exception {
         from("kafka:camel-topic")
+                .process(processor)
                 .log("${body}");
     }
 }
